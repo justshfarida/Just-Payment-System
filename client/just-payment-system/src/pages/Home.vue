e5cc0510c5b8fb174ab77a566613e7d9bc177d76
NizoDotNet <mmmdovnizami.official@gmail.com>
2026-05-19 23:52:07 +0400

    tailwind

---
+++ client/just-payment-system/src/pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { IconHomeFilled } from '@tabler/icons-vue'
+</script>
+
+<template>
+  <div class="bg-red-300">
+    <h1>This is home page</h1>
+    <IconHomeFilled />
+  </div>
+</template>
