ecbb88012ba71f84ed9b7d89eda8362f0040d992
NizoDotNet <mmmdovnizami.official@gmail.com>
2026-05-20 22:12:52 +0400

    nuxt-ui

--- client/just-payment-system/vite.config.ts
+++ client/just-payment-system/vite.config.ts
@@ -4,10 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import ui from '@nuxt/ui/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), vueDevTools(), tailwindcss(), ui()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
