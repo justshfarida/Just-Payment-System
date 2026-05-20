25afbaf398bee19ee1e3d16d4555c7b45d0f7220
NizoDotNet <mmmdovnizami.official@gmail.com>
2026-05-20 21:42:23 +0400

    keycloak login page

--- client/just-payment-system/src/App.vue
+++ client/just-payment-system/src/App.vue
@@ -1,4 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { keycloak } from './keycloak'
+
+onMounted(async () => {
+  await keycloak.init({
+    onLoad: 'login-required',
+  })
+})
+</script>
 
 <template>
   <RouterView></RouterView>
