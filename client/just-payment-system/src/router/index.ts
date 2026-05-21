a1e2cb89f7ead51a26d7d354196aaf575be9cbe3
NizoDotNet <mmmdovnizami.official@gmail.com>
2026-05-21 10:44:50 +0400

    folder based routing

--- client/just-payment-system/src/router/index.ts
+++ client/just-payment-system/src/router/index.ts
@@ -1,14 +1,14 @@
 import Home from '@/pages/Home.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { routes, handleHotUpdate } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      component: Home,
-    },
-  ],
+  routes,
 })
+
+if (import.meta.hot) {
+  handleHotUpdate(router)
+}
 
 export default router
--- client/just-payment-system/vite.config.ts
+++ client/just-payment-system/vite.config.ts
@@ -3,12 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
+import vueRouter from 'vue-router/vite'
 import ui from '@nuxt/ui/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), ui()],
+  plugins: [
+    vueRouter({
+      dts: 'src/route-map.d.ts',
+    }),
+    vue(),
+    vueDevTools(),
+    ui(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
