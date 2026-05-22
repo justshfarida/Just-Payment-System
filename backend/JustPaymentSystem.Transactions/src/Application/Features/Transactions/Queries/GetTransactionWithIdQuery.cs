29599ee7aacf46b2af0c5a09637be59685549bec
NizoDotNet <mmmdovnizami.official@gmail.com>
2026-05-22 14:55:24 +0400

    transaction queries

---
+++ backend/JustPaymentSystem.Transactions/src/Application/Features/Transactions/Queries/GetTransactionWithIdQuery.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.Features.Transactions.Queries.DTOs;
+
+namespace Application.Features.Transactions.Queries;
+
+public sealed record GetTransactionByIdQuery(Guid Id);
+
+public sealed class GetTransactionByIdHandler
+{
+    private readonly ITransactionReadRepository _transactionReadRepository;
+
+    public GetTransactionByIdHandler(
+        ITransactionReadRepository transactionReadRepository)
+    {
+        _transactionReadRepository = transactionReadRepository;
+    }
+
+    public async Task<TransactionResponse> Handle(
+        GetTransactionByIdQuery query,
+        CancellationToken cancellationToken)
+    {
+        var transaction = await _transactionReadRepository
+            .GetByIdAsync(query.Id, cancellationToken);
+
+        if (transaction is null)
+        {
+            throw new NotFoundException(
+                $"Transaction with id '{query.Id}' was not found.");
+        }
+
+        return transaction;
+    }
+}
---
+++ backend/JustPaymentSystem.Transactions/src/Application/Features/Transactions/Queries/GetTransactionsQuery.cs
@@ -0,0 +1,22 @@
+﻿using Application.Common.Interfaces;
+using Application.Features.Transactions.Queries.DTOs;
+using Domain.Shared.Enums;
+
+namespace Application.Features.Transactions.Queries;
+
+public sealed record GetTransactionsQuery(int Page, int PageSize, Guid? MerchantId, string? Currency, TransactionStatus? Status);
+
+public sealed class GetTransactionsQueryHandler
+{
+    private readonly ITransactionReadRepository _transactionReadRepository;
+
+    public GetTransactionsQueryHandler(ITransactionReadRepository transactionReadRepository)
+    {
+        _transactionReadRepository = transactionReadRepository;
+    }
+
+    public async Task<List<TransactionResponse>> Handle(GetTransactionsQuery request, CancellationToken cancellationToken)
+    {
+        return await _transactionReadRepository.GetAllWithPaginationAsync(request.Page, request.PageSize, request.MerchantId, request.Currency, request.Status);
+    }
+}
