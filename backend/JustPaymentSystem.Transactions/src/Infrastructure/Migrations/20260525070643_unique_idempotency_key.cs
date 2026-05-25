e398a3c384bc8b06e8afc6bdd73918ac7b38c156
NizoDotNet <mmmdovnizami.official@gmail.com>
2026-05-25 11:07:15 +0400

    migration uplied

---
+++ backend/JustPaymentSystem.Transactions/src/Infrastructure/Migrations/20260525070643_unique_idempotency_key.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class unique_idempotency_key : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_IdempotencyKey",
+                table: "Transactions",
+                column: "IdempotencyKey",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_IdempotencyKey",
+                table: "Transactions");
+        }
+    }
+}
