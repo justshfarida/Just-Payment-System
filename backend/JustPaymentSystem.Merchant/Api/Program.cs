dbd4fb5ede08d711de666c11a97f907dabf93a11
shakikhanli <farida.shakikhanli@etu.unistra.fr>
2026-05-29 12:01:12 +0400

    Add initial migration

--- backend/JustPaymentSystem.Merchant/Api/Program.cs
+++ backend/JustPaymentSystem.Merchant/Api/Program.cs
@@ -1,4 +1,7 @@
 
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 namespace Api
 {
     public class Program
@@ -8,6 +11,8 @@ namespace Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<MerchantDbContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
